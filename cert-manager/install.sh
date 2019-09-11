1d899372f8241c516e0e20f7328f90768374ea78
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-09-11 12:55:52 +0100

    Add cert-manager
    Essential for TLS certificates on k3s.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ cert-manager/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install the CustomResourceDefinition resources separately
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.10/deploy/manifests/00-crds.yaml
+
+# Create the namespace for cert-manager
+kubectl create namespace cert-manager
+
+# Label the cert-manager namespace to disable resource validation
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+
+# Add the Jetstack Helm repository
+helm repo add jetstack https://charts.jetstack.io
+
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the cert-manager Helm chart
+helm install \
+  --name cert-manager \
+  --namespace cert-manager \
+  --version v0.10.0 \
+  jetstack/cert-manager
---
+++ cert-manager/post_install.md
@@ -0,0 +1,9 @@
+## cert-manager - a native Kubernetes certificate management controller
+
+### Get started
+
+[cert-manager docs](https://cert-manager.readthedocs.io/en/latest/)
+
+* Create an [Issuer](https://docs.cert-manager.io/en/latest/tutorials/acme/index.html?highlight=issuer)
+
+* Create a [Certificate or Ingress record](https://docs.cert-manager.io/en/latest/tasks/issuing-certificates/index.html)
