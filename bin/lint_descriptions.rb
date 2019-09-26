070e5a0bf19cb00837f3df1f7de16742764bd87a
Andy Jeffries <andy@andyjeffries.co.uk>
2019-09-26 09:45:46 +0100

    Add checks for salesy words - more to follow

--- bin/lint_descriptions.rb
+++ bin/lint_descriptions.rb
@@ -13,6 +13,11 @@ Dir.glob('**/*.yaml').each do |filename|
       puts "#{filename} has too a long description (#{yaml["description"].length} chars, 160 max)"
       exit_code = 1
     end
+
+    if matches = yaml["description"].scan(/(best|award[- ]winning|leading|greatest|super)/)
+      puts "#{filename} has salesy terms in the description - #{matches.flatten.join(", ")}"
+      exit_code = 2
+    end
   end
 end
 
