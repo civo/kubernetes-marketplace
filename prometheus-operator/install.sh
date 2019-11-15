a586afd2bf48f748ffcb632bc05873de5875feb1
Gabriel Duke <gabeduke@gmail.com>
2019-11-15 14:04:30 -0500

    remove modified helm cmd

--- prometheus-operator/install.sh
+++ prometheus-operator/install.sh
@@ -6,11 +6,12 @@ HELM_CHART_VERSION=8.2.0
 kubectl create namespace ${NAMESPACE}
 
 # Update your local Helm chart repository cache
-helm3 repo update
+helm repo update
 
 # Install the prometheus-operator Helm chart
-helm3 install \
+helm install \
+  --name prometheus-operator \
   --namespace ${NAMESPACE} \
   --set prometheusOperator.createCustomResource=false \
   --version ${HELM_CHART_VERSION} \
-  prometheus-operator stable/prometheus-operator
+  stable/prometheus-operator
