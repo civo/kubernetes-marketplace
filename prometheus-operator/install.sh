88fd3350502b8534497a8f75b4ec1bef5034a8b2
Gabriel Duke <gabeduke@gmail.com>
2019-11-19 21:08:52 -0500

    fix app version, add wait loop for crds

--- prometheus-operator/install.sh
+++ prometheus-operator/install.sh
@@ -8,6 +8,21 @@ kubectl create namespace ${NAMESPACE}
 # Update your local Helm chart repository cache
 helm repo update
 
+# Ensure CRDs exist
+checkCRDs () {
+  kubectl -n ${NAMESPACE} wait --for condition=established --timeout=60s crd/alertmanagers.monitoring.coreos.com
+  kubectl -n ${NAMESPACE} wait --for condition=established --timeout=60s crd/podmonitors.monitoring.coreos.com
+  kubectl -n ${NAMESPACE} wait --for condition=established --timeout=60s crd/prometheuses.monitoring.coreos.com
+  kubectl -n ${NAMESPACE} wait --for condition=established --timeout=60s crd/prometheusrules.monitoring.coreos.com
+  kubectl -n ${NAMESPACE} wait --for condition=established --timeout=60s crd/servicemonitors.monitoring.coreos.com
+}
+
+BACKOFF=1
+until checkCRDs || [ $BACKOFF -eq 10 ]; do
+  printf 'Waiting for CRDs to exist...'
+  sleep $(( BACKOFF++ ))
+done
+
 # Install the prometheus-operator Helm chart
 helm install \
   --name prometheus-operator \
