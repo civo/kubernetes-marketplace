5a29ec2fffaf00d28d94f6dc019857db1dde0b8d
Gabriel Duke <gabeduke@gmail.com>
2019-11-24 21:22:40 -0500

    use helm crd

--- prometheus-operator/uninstall.sh
+++ prometheus-operator/uninstall.sh
@@ -3,8 +3,5 @@
 # Delete the custom CRDs
 kubectl delete -f app.yaml
 
-# Delete the helm app
-helm delete --purge prometheus-operator
-
 # Delete the namaespace
 kubectl delete namespace monitoring
