9286fec722b5bb1a97b2cfcd6fb011594dd3827a
Andy Jeffries <andy@andyjeffries.co.uk>
2019-12-06 13:06:54 +0000

    Remove default from local path storage when Longhorn added

--- longhorn/install.sh
+++ longhorn/install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 kubectl patch storageclass longhorn -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
