ac32b4140c3cb781d2540d32d1d57296d58ef435
Andy Jeffries <andy@andyjeffries.co.uk>
2020-04-23 13:07:19 +0100

    Change metrics-server to a default app, as it is now a k3s default

--- metrics-server/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-helm repo update
-
-# Work-around from OpenFaaS docs - https://docs.openfaas.com/tutorials/kubernetes-hpa/
-helm install --name metrics-server --namespace kube-system \
-  stable/metrics-server \
-  --set args="{--kubelet-insecure-tls,--kubelet-preferred-address-types=InternalIP,ExternalIP,Hostname}"
