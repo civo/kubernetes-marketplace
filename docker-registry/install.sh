840d5487adf851c71761b2fe77d988e3b7532e88
Alejandro JNM <alejandrojnm@gmail.com>
2020-07-29 09:49:56 -0400

    Added a docker registry to the marketplace
    Signed-off-by: Alejandro JNM <alejandrojnm@gmail.com>

---
+++ docker-registry/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+htpasswd -Bbn admin admin > auth
+htpasswd -Bbn $REGISTRY_USERNAME $REGISTRY_PASSWD > /tmp/auth
+
+kubectl create secret generic auth-ingress --from-file /tmp/auth
+
+# Update the helm repo
+helm repo update
+
+helm install stable/docker-registry --name private-registry \
+  --namespace default --set persistence.enabled=false
