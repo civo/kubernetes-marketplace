0507f08f034e45a2404ab87a4cb7cf6608423a05
Sangam Biradar <sangam14@users.noreply.github.com>
2020-09-20 19:17:04 +0530

    Update install.sh

--- keptn/install.sh
+++ keptn/install.sh
@@ -6,14 +6,6 @@ keptn install --endpoint-service-type=ClusterIP --use-case=continuous-delivery
 
 kubectl get deployments -n keptn
 
-kubectl -n keptn port-forward service/api-gateway-nginx 8080:80
-
-KEPTN_ENDPOINT=http://localhost:8080/api
-KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath='{.data.keptn-api-token}' | base64 --decode)
-
-keptn auth --endpoint=$KEPTN_ENDPOINT --api-token=$KEPTN_API_TOKEN
-
-keptn configure bridge --output
 
 
 
