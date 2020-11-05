ff2b7723bc91f75da2818c222b086d8024fd3de5
keithhubner <32544724+keithhubner@users.noreply.github.com>
2020-11-05 18:16:34 +0000

    Create civo-system namespace, create mysql-pass secret in this namespace and then copy to default namespace

--- mariadb/pre_install.sh
+++ mariadb/pre_install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+cat <<EOF | kubectl create namespace civo-system
+EOF
+
 cat <<EOF | kubectl create -f -
 apiVersion: v1
 kind: Secret
 metadata:
   name: mysql-pass
+  namespace: civo-system
 type: Opaque
 stringData:
   MYSQL_ROOT_PASSWORD: "$MYSQL_ROOT_PASSWORD" # password used mysql root
 EOF
+
+cat <<EOF | kubectl get secret mysql-pass --namespace=civo-system -oyaml | grep -v '^\s*namespace:\s' | kubectl apply --namespace=default -f -
+
+EOF   
