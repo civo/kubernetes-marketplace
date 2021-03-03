cc7b67cc53dfcbe152ddcd928713569b43cb5b83
Saiyam Pathak <saiyam@civo.com>
2021-03-03 16:45:37 +0530

    Update install.sh

--- kubevious/install.sh
+++ kubevious/install.sh
@@ -1,3 +1,3 @@
 kubectl create namespace kubevious
 helm repo add kubevious https://helm.kubevious.io
-helm upgrade --atomic -i -n kubevious --version 0.6.36  --set ingress.enabled=true  kubevious kubevious/kubevious
+helm upgrade --atomic -i -n kubevious --version 0.8.15 --set ingress.enabled=true  kubevious kubevious/kubevious
