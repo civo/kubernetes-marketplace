8fcdf38cd042bbd9b50a741fd4d2d80c45b10fa8
Zulhilmi Zainudin <zulh@civo.com>
2021-03-16 16:46:05 +0800

    Update MariaDB app — add 'mariadb' namespace

--- mariadb/pre_install.sh
+++ mariadb/pre_install.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-cat <<EOF | kubectl create namespace civo-system
-EOF
-
-cat <<EOF | kubectl create -f -
+kubectl apply -f - <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
   name: mysql-pass
-  namespace: civo-system
+  namespace: mariadb
 type: Opaque
 stringData:
   MYSQL_ROOT_PASSWORD: "$MYSQL_ROOT_PASSWORD" # password used mysql root
-EOF
-
-cat <<EOF | kubectl get secret mysql-pass --namespace=civo-system -oyaml | grep -v '^\s*namespace:\s' | kubectl apply --namespace=default -f -
-
 EOF
