c4cb70ec928731a86d0fd77d8297540ce51d7079
Zulhilmi Zainudin <zulh@civo.com>
2021-04-08 00:39:14 +0800

    Create a kubeconfig file from daemon's service account - take 2

--- keptn/uninstall.sh
+++ keptn/uninstall.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-curl -sL https://get.keptn.sh | sudo -E bash
+# ============================================================================================
+# Create a kubeconfig file from daemon's service account
+# https://docs.oracle.com/en-us/iaas/Content/ContEng/Tasks/contengaddingserviceaccttoken.htm
+# ============================================================================================
+TOKENNAME=`kubectl -n kubemart-system get serviceaccount/kubemart-daemon-svc-acc -o jsonpath='{.secrets[0].name}'`
+echo "TOKENNAME:" $TOKENNAME
 
+TOKEN=`kubectl -n kubemart-system get secret $TOKENNAME -o jsonpath='{.data.token}'| base64 --decode`
+echo "TOKEN:" $TOKEN
+
+kubectl config set-credentials kubemart-daemon-svc-acc --token=$TOKEN
+kubectl config set-context ephemeral --user=kubemart-daemon-svc-acc
+kubectl config use-context ephemeral
+
+# ============================================================================================
+# Uninstall Keptn
+# ============================================================================================
+curl -sL https://get.keptn.sh | sudo -E bash
 keptn uninstall --yes
