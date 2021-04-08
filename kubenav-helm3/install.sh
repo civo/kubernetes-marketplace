b00566087c3e7334e676e6522a276bd9ee816cf6
Zulhilmi Zainudin <zulh@civo.com>
2021-04-08 17:40:36 +0800

    Delete kubenav-helm3 app (redundant)

--- kubenav-helm3/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-helm repo add kubenav https://kubenav.github.io/helm-repository
-helm repo update 
-helm install kubenav kubenav/kubenav --namespace kubenav
--- kubenav-helm3/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Delete the app
-helm uninstall kubenav --namespace kubenav
-
-# Delete the namaespace
-kubectl delete namespace kubenav
