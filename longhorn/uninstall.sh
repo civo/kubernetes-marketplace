620cd597864aa35d280cbb7b10d85520d73bd1b6
Zulhilmi Zainudin <zulh@civo.com>
2021-04-10 11:10:30 +0800

    Update longhorn uninstall script - take 6

--- longhorn/uninstall.sh
+++ longhorn/uninstall.sh
@@ -4,9 +4,12 @@ kubectl patch storageclass longhorn -p '{"metadata": {"annotations":{"storagecla
 
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
-# https://longhorn.io/docs/1.0.0/deploy/uninstall/#uninstalling-longhorn-using-kubectl
+# https://longhorn.io/docs/1.1.0/deploy/uninstall/#uninstalling-longhorn-using-kubectl
 kubectl create -f https://raw.githubusercontent.com/longhorn/longhorn/v1.1.0/uninstall/uninstall.yaml
 
 kubectl wait --for=condition=complete --timeout=300s job/longhorn-uninstall
 
 kubectl get crds | grep longhorn | cut -d' ' -f1 | xargs -I % sh -c 'kubectl delete crd %'
+
+# https://longhorn.io/docs/1.1.0/deploy/uninstall/#uninstalling-longhorn-using-kubectl
+kubectl delete -f https://raw.githubusercontent.com/longhorn/longhorn/v1.1.0/uninstall/uninstall.yaml
