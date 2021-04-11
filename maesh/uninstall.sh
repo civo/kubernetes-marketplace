2d2ca72e9900977e91ea20b39808c1e8bc84ba0c
Zulhilmi Zainudin <zulh@civo.com>
2021-04-12 01:16:29 +0800

    Delete maesh CRDs when uninstall the app

--- maesh/uninstall.sh
+++ maesh/uninstall.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Uninstall
 helm --namespace maesh delete maesh
+
+# Delete CRDs
+helm pull https://containous.github.io/maesh/charts maesh/maesh
+TAR_FILE=$(ls | grep maesh*.tgz)
+echo "Chart file:" $TAR_FILE
+tar -xvf $TAR_FILE
+kubectl delete -f ./maesh/crds
