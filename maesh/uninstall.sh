dc93c869b56df08007e7ccb9c4ecef7d0e448d88
Zulhilmi Zainudin <zulh@civo.com>
2021-04-12 01:40:04 +0800

    Add maesh chart before pulling its contents

--- maesh/uninstall.sh
+++ maesh/uninstall.sh
@@ -4,6 +4,8 @@
 helm --namespace maesh delete maesh
 
 # Delete CRDs
+helm repo add maesh https://containous.github.io/maesh/charts
+helm repo update
 helm pull https://containous.github.io/maesh/charts maesh/maesh
 TAR_FILE=$(ls | grep maesh*.tgz)
 echo "Chart file:" $TAR_FILE
