b212131ee82d0a83bb5415b3e9c338a29b014a62
jasonmorgan <jmorgan@f9vs.com>
2021-04-16 18:26:16 -0400

    deleting viz and jaeger.

--- linkerd-viz/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -sL https://run.linkerd.io/install | sh
-
-export PATH=$PATH:$HOME/.linkerd2/bin
-
-linkerd check && linkerd viz install | kubectl apply -f -
-linkerd check || exit 1
