1a83a063af747d1489b2b74b91834f1a98cc79bd
ksatchit <karthik.s@mayadata.io>
2021-04-19 03:13:22 +0000

    (chore)litmuschaos: update litmus charts to 2.0beta
    Signed-off-by: ksatchit <karthik.s@mayadata.io>

--- litmuschaos/install.sh
+++ litmuschaos/install.sh
@@ -1,3 +1,4 @@
 kubectl create ns litmus
 helm repo add litmuschaos https://litmuschaos.github.io/litmus-helm
-helm install chaos litmuschaos/litmus --namespace=litmus
+helm search repo litmuschaos --devel
+helm install chaos litmuschaos/litmus-2-0-0-beta --devel --namespace=litmus
