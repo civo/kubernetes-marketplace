0a23d6de79e4d3dbd52cd38b3bc10241410ad5d7
jasonmorgan <jmorgan@f9vs.com>
2021-04-19 09:05:46 -0400

    Added plans to Linkerd.

--- linkerd/install.sh
+++ linkerd/install.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
 curl -sL https://run.linkerd.io/install | sh
-
 export PATH=$PATH:$HOME/.linkerd2/bin
 
 linkerd check --pre && linkerd install | kubectl apply -f -
-linkerd check || exit 1
+case $LINKERD in
+  linkerd)
+    linkerd check || exit 1
+    ;;
+  linkerdjaeger)  
+    linkerd check || exit 1
+    linkerd jaeger install | kubectl apply -f -
+    linkerd check || exit 1
+    ;;
+  linkerdviz)
+    linkerd check || exit 1
+    linkerd viz install | kubectl apply -f -
+    linkerd check || exit 1
+    ;;
+  theworks)
+    linkerd check || exit 1
+    linkerd viz install | kubectl apply -f -
+    linkerd check || exit 1
+    linkerd jaeger install | kubectl apply -f -
+    linkerd check || exit 1
+    ;;
+esac
