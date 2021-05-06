5d315a65b089ac1b155aa55ebfb67a4d19d7c4f8
Dinesh Majrekar <dinesh@civo.com>
2021-05-06 11:16:22 +0100

    Disable CertManager web hooks

--- cert-manager/install.sh
+++ cert-manager/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml
+kubectl delete -A ValidatingWebhookConfiguration cert-manager-webhook
+kubectl delete -A mutatingwebhookconfigurations cert-manager-webhook
