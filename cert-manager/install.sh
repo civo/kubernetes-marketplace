6a1b3dda6fbeba44d26c93dc2167e32d105dcf41
Saiyam Pathak <saiyam@civo.com>
2021-05-07 10:37:51 +0530

    Update version to v1.3.1

--- cert-manager/install.sh
+++ cert-manager/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml
-kubectl delete -A ValidatingWebhookConfiguration cert-manager-webhook
-kubectl delete -A mutatingwebhookconfigurations cert-manager-webhook
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
