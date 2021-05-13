cb919b15f00d63e8208a34d107b029d85b08908d
TannerGabriel <gabrieltanner.code@gmail.com>
2021-05-13 17:52:11 +0200

    Update Keptn extension to V0.8.2

--- keptn/install.sh
+++ keptn/install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-curl -sL https://get.keptn.sh | sudo -E bash
-
-echo "{}" > creds.json
-
-keptn install --endpoint-service-type=NodePort -c ./creds.json
-
+helm upgrade keptn keptn --install -n keptn --create-namespace --wait --version=0.8.2 --repo=https://storage.googleapis.com/keptn-installer --set=control-plane.apiGatewayNginx.type=LoadBalancer
