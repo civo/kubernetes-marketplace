9d27948e0207408e4cb89d1ab23b5a811af1309f
Frank Jogeleit <frank.jogeleit@web.de>
2021-05-14 13:28:16 +0200

    Add Plan for Pod Security Standard and Validation Failure Action

--- kyverno/install.sh
+++ kyverno/install.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
 helm repo add kyverno https://kyverno.github.io/kyverno/
 
 helm repo update
 
-helm install kyverno kyverno/kyverno --namespace kyverno --create-namespace
+VALUES=""
+
+case $POD_SECURITY_STANDARDS in
+  default_enforce)
+    VALUES="--set validationFailureAction=enforce"
+    ;;
+  restricted_audit)
+    VALUES="--set podSecurityStandard=restricted"
+    ;;
+  restricted_enforce)
+    VALUES="--set validationFailureAction=enforce --set podSecurityStandard=restricted"
+    ;;
+esac
+
+helm install kyverno kyverno/kyverno $VALUES --namespace kyverno --create-namespace
