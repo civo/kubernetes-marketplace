36eadae7d2aba98f9462bbae8c464c47f27b97a9
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-05-26 13:37:05 +0530

    remove traefik unistall

--- gloo-edge/install.sh
+++ gloo-edge/install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 GLOO_NS=gloo-system
-TRAEFIK_NS=kube-system
-
-# Uninstall default Trafeik resources as gloo-edge and traefik 
-# are mutally exclusive
-
-kubectl config set-context --current --namespace=$TRAEFIK_NS
-helm uninstall traefik
 
 # Get back to default NS
 kubectl config set-context --current --namespace=default
