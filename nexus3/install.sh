be5e13c42cbdfcc86acbb7fd9a52ba83f6df2563
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-05-26 16:56:17 +0530

    Use civo repo

--- nexus3/install.sh
+++ nexus3/install.sh
@@ -7,7 +7,7 @@ kubectl config set-context --current --namespace=$NEXUS_NS
 helm repo add sonatype https://sonatype.github.io/helm3-charts/
 helm repo update
 
-helm install -f https://raw.githubusercontent.com/kameshsampath/kubernetes-marketplace/master/nexus3/values.yaml --wait \
+helm install -f https://raw.githubusercontent.com/civo/kubernetes-marketplace/master/nexus3/values.yaml --wait \
   nexus3 sonatype/nexus-repository-manager
 
 # Set context namespace back to "default"
