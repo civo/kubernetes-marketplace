e865061627f2ffa609aa790861709c720fdb6494
TannerGabriel <gabrieltanner.code@gmail.com>
2021-06-01 14:55:20 +0200

    Installing Keptn Version 0.8.3

--- keptn/install.sh
+++ keptn/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-helm upgrade keptn keptn --install -n keptn --create-namespace --wait --version=0.8.2 --repo=https://storage.googleapis.com/keptn-installer --set=control-plane.apiGatewayNginx.type=LoadBalancer
+helm upgrade keptn keptn --install -n keptn --create-namespace --wait --version=0.8.3 --repo=https://storage.googleapis.com/keptn-installer --set=control-plane.apiGatewayNginx.type=LoadBalancer --timeout 10m
