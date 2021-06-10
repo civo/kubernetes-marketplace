29802b867043f08d5e0c169d2d743e7dd9a3023e
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-06-10 12:28:00 +0530

    use istio tar ball over downlod script

--- istio/install.sh
+++ istio/install.sh
@@ -1,28 +1,72 @@
 #!/bin/bash
-
-set -eu 
+set -e
 set -o pipefail 
 
 ISTIO_NS=istio-system
+ISTIO_INGRESS_NS=istio-ingress
 
-if [ "$ISTIO_VERSION" == "latest" ];
-then
-  ISTIO_VERSION=$(curl -sSL https://api.github.com/repos/istio/istio/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-  echo "Downloading latest Istio: v$ISTIO_VERSION"
-  curl -L https://istio.io/downloadIstio | sh -
+# Determines the operating system.
+OS="$(uname)"
+if [ "${OS}" = "Darwin" ] ; then
+  OSEXT="osx"
 else
-  echo "Downloading Istio: v$ISTIO_VERSION"
-  curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION sh -
+  OSEXT="linux"
 fi
 
-ISTIO_DIR=$(find . -name istio*)
+# Determin Istio version to download
+if [ -z "$ISTIO_VERSION" ] || [ "$ISTIO_VERSION" == "latest" ];
+then
+  ISTIO_VERSION="$(curl -sL https://github.com/istio/istio/releases | \
+                  grep -o 'releases/[0-9]*.[0-9]*.[0-9]*/' | sort --version-sort | \
+                  tail -1 | awk -F'/' '{ print $2}')"
+  ISTIO_VERSION="${ISTIO_VERSION##*/}"
+fi
+
+echo "Downloading Istio Version $ISTIO_VERSION"
+
+DOWNLOAD_URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz"
+
+wget -q "${DOWNLOAD_URL}" -O - | tar -zxf -
+
+ISTIO_DIR="istio-$ISTIO_VERSION"
 export PATH=$ISTIO_DIR/bin:$PATH
 
-# # Run prechecks
-istioctl x precheck
+# Create a revision name
+ISTIO_REVISION="${ISTIO_VERSION//./-}"
 
-# # Install Istio Demo Profile
-istioctl install --set profile=demo -y
+NS=$(kubectl get namespace $ISTIO_NS --ignore-not-found);
+if [[ "$NS" ]]; then
+  echo "Skipping creation of namespace $ISTIO_NS - already exists";
+else
+  echo "Creating namespace $ISTIO_NS";
+  kubectl create namespace $ISTIO_NS;
+fi;
 
-# Create the Ingress Gateway for istio-ingressgateway
-kubectl apply -n $ISTIO_NS https://raw.githubusercontent.com/civo/kubernetes-marketplace/master/istio/ingress.yaml
+SVC=$(kubectl get svc istiod -n $ISTIO_NS --ignore-not-found);
+if [[ "$SVC" ]]; then
+  echo "Skipping creation of service istiod - already exists";
+else
+  echo "Creating service istiod";
+  kubectl create -n $ISTIO_NS \
+  -f https://raw.githubusercontent.com/civo/kubernetes-marketplace/master/istio/istiod-service.yaml
+fi;
+
+istioctl install -y -n $ISTIO_NS \
+  -f control-plane.yaml \
+  --revision "$ISTIO_REVISION"
+
+###########################################
+# Deploy Ingress Gateways
+###########################################
+
+NS=$(kubectl get namespace $ISTIO_INGRESS_NS --ignore-not-found);
+if [[ "$NS" ]]; then
+  echo "Skipping creation of namespace $ISTIO_INGRESS_NS - already exists";
+else
+  echo "Creating namespace $ISTIO_INGRESS_NS";
+  kubectl create namespace $ISTIO_INGRESS_NS;
+fi;
+
+istioctl install -y -n $ISTIO_INGRESS_NS \
+   -f https://raw.githubusercontent.com/civo/kubernetes-marketplace/master/istio/ingress-gateways.yaml \
+  --revision "$ISTIO_REVISION"
--- istio/uninstall.sh
+++ istio/uninstall.sh
@@ -4,23 +4,43 @@ set -eu
 set -o pipefail 
 
 ISTIO_NS=istio-system
+ISTIO_INGRESS_NS=istio-ingress
 
-if [ "$ISTIO_VERSION" == "latest" ];
-then
-  unset ISTIO_VERSION
-  curl -L https://istio.io/downloadIstio | sh -
+# Determines the operating system.
+OS="$(uname)"
+if [ "${OS}" = "Darwin" ] ; then
+  OSEXT="osx"
 else
-  curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION sh -
+  OSEXT="linux"
 fi
 
-ISTIO_DIR=$(find . -name istio*)
+# Determin Istio version to download
+if [ -z "$ISTIO_VERSION" ] || [ "$ISTIO_VERSION" == "latest" ];
+then
+  ISTIO_VERSION="$(curl -sL https://github.com/istio/istio/releases | \
+                  grep -o 'releases/[0-9]*.[0-9]*.[0-9]*/' | sort --version-sort | \
+                  tail -1 | awk -F'/' '{ print $2}')"
+  ISTIO_VERSION="${ISTIO_VERSION##*/}"
+fi
+
+echo "Downloading Istio Version $ISTIO_VERSION"
+
+DOWNLOAD_URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz"
+
+wget -q "${DOWNLOAD_URL}" -O - | tar -zxf -
+
+ISTIO_DIR="istio-$ISTIO_VERSION"
 export PATH=$ISTIO_DIR/bin:$PATH
 
-kubectl config set-context --current --namespace=$ISTIO_NS
-
-# Delete Istio
+# Delete Istio CRD and other resources
 istioctl manifest generate --set profile=demo |\
    kubectl delete  -n$ISTIO_NS --ignore-not-found=true -f -
+
+# Delete Istio Ingress Namespace
+kubectl delete ns $ISTIO_INGRESS_NS --ignore-not-found=true
+
+# Delete Istio Namespace
+kubectl delete ns $ISTIO_NS --ignore-not-found=true
 
 # Set context namespace back to "default"
 kubectl config set-context --current --namespace=default
