9bf3638f5ce6aa8b8e0462003e4e9761ca3d6337
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-06-28 20:24:13 +0530

    add a wait for downloadIstio to complete

--- istio/install.sh
+++ istio/install.sh
@@ -16,7 +16,8 @@ fi
 
 echo "Downloading Istio Version $ISTIO_VERSION"
 
-curl -L https://istio.io/downloadIstio | sh -
+curl -L https://istio.io/downloadIstio | sh - &
+wait 
 
 ###########################################
 # Deploy Control Plane
