489ca81cc44c1bf0c8b86bef5fee1eedf8923b6b
Saiyam Pathak <saiyam@civo.com>
2021-08-02 10:24:31 +0530

    adding dev as install option

--- vault/install.sh
+++ vault/install.sh
@@ -1,3 +1,11 @@
 kubectl create namespace vault
 helm repo add hashicorp https://helm.releases.hashicorp.com
+VALUES=""
+
+case $INSTALL_MODE in
+  dev)
+    VALUES='--set "server.dev.enabled=true"'
+    ;;
+esac
+
 helm upgrade --atomic --install --namespace vault --version 0.13.0 vault hashicorp/vault
