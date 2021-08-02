d3b081a5295e751596d00debd57992648231ff39
Saiyam Pathak <saiyam@civo.com>
2021-08-02 11:20:15 +0530

    Update install.sh

--- vault/install.sh
+++ vault/install.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+
 kubectl create namespace vault
 helm repo add hashicorp https://helm.releases.hashicorp.com
-VALUES=""
 
 case $INSTALL_MODE in
   dev)
-    VALUES='--set "server.dev.enabled=true"'
+    helm upgrade --install --namespace vault  --version 0.13.0 vault hashicorp/vault --set "server.dev.enabled=true"
+    ;;
+  standalone)
+    helm upgrade --atomic --install --namespace vault --version 0.13.0 vault hashicorp/vault
     ;;
 esac
 
-helm upgrade --atomic --install --namespace vault --version 0.13.0 vault hashicorp/vault
