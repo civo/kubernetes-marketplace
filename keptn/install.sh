39624c60cbe493e8138f2f5257ad4b24f8c2b218
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2021-08-09 10:04:30 +0200

    upgrade to keptn 0.8.7
    Signed-off-by: jetzlstorfer <juergen.etzlstorfer@dynatrace.com>

--- keptn/install.sh
+++ keptn/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download the Keptn helm chart
-curl -o keptn.tgz https://storage.googleapis.com/keptn-installer/keptn-0.8.3.tgz 
+curl -o keptn.tgz https://storage.googleapis.com/keptn-installer/keptn-0.8.7.tgz 
 
 # install the downloaded keptn helm chart
-helm upgrade keptn keptn.tgz --install -n keptn --create-namespace --wait --version=0.8.3 --set=control-plane.apiGatewayNginx.type=LoadBalancer --timeout 10m
+helm upgrade keptn keptn.tgz --install -n keptn --create-namespace --wait --version=0.8.7 --set=control-plane.apiGatewayNginx.type=LoadBalancer --timeout 10m
