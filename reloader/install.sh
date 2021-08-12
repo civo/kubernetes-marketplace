421393f6eb55cde8e064f96b1992f62b81e1b4b7
Douglas Simon Camargo <douglas_simon@hotmail.com>
2021-08-12 16:03:20 -0300

    create namespace in helm via flag

--- reloader/install.sh
+++ reloader/install.sh
@@ -3,6 +3,4 @@
 helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo update
 
-kubectl create namespace reloader
-
-helm install reloader stakater/reloader --version v0.0.91 --namespace reloader
+helm install reloader stakater/reloader --version v0.0.91 --namespace reloader --create-namespace
