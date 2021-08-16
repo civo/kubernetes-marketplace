e1838551d8c983153cb0aa97aaed4e85ecb82017
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2021-08-16 18:45:02 +0530

    added helm chart version
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- litmuschaos/install.sh
+++ litmuschaos/install.sh
@@ -1,3 +1,3 @@
 kubectl create ns litmus
 helm repo add litmuschaos https://litmuschaos.github.io/litmus-helm
-helm install chaos litmuschaos/litmus --namespace=litmus
+helm install chaos litmuschaos/litmus --namespace=litmus --version=2.0.28
