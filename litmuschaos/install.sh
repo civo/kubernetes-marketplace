3b798e4bea34ea4c9ba8f836440cb375f1a58240
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2021-08-16 17:29:47 +0530

    updated litmus to 2.0.0 version
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- litmuschaos/install.sh
+++ litmuschaos/install.sh
@@ -1,4 +1,3 @@
 kubectl create ns litmus
 helm repo add litmuschaos https://litmuschaos.github.io/litmus-helm
-helm search repo litmuschaos --devel
-helm install chaos litmuschaos/litmus-2-0-0-beta --devel --namespace=litmus
+helm install chaos litmuschaos/litmus --namespace=litmus
