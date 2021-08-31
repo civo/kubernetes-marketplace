29e847b2a0919fac26fefe5221fce7e9ee573d82
Saiyam Pathak <saiyam@civo.com>
2021-08-31 18:53:50 +0530

    Rancher fix

--- rancher/install.sh
+++ rancher/install.sh
@@ -3,8 +3,8 @@
 subdomain="$CLUSTER_ID.k8s.civo.com"
 email="$EMAIL"
 
-kubectl wait --namespace=cert-manager --for=condition=Ready pods --selector app.kubernetes.io/instance=cert-manager --timeout=200s
-
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
+sleep 30
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 helm repo update
 kubectl create namespace cattle-system
