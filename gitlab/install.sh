2e989925de7f8f7f3ada6f2b23f7b4031c396442
Saiyam Pathak <saiyam@civo.com>
2021-11-09 06:58:52 +0530

    New App - GitLab

---
+++ gitlab/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+domain="$CLUSTER_ID.k8s.civo.com"
+email="$EMAIL"
+edition=$EDITION
+
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+
+kubectl create namespace gitlab --dry-run=client -o yaml | kubectl apply -f - 
+
+echo 'waiting for 30s for the components to get started'
+sleep 30
+
+helm upgrade --install gitlab gitlab/gitlab --namespace gitlab --set global.edition="$edition" --set global.hosts.domain=="$domain" --set certmanager-issuer.email="$email" --timeout 600s --version 5.4.0 -f https://raw.githubusercontent.com/civo/kubernetes-marketplace/master/gitlab/config.yaml 
+
