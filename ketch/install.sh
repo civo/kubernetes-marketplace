086d520f2d2e0d8ba2038287faaf31a277f29e81
Ravi Lachhman <42775804+ravilach@users.noreply.github.com>
2021-11-16 12:45:15 -0500

    Patched Ketch Version and sleep for 5 seconds for Cert Manager Replica

--- ketch/install.sh
+++ ketch/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#Sleep for 5 Seconds incase Civo Cert Manager Install has not scaled to one replica. Ketch v0.6.1 handles this better.
+sleep 5
+
 #Install Ketch
-kubectl apply -f https://github.com/theketchio/ketch/releases/download/v0.6.0/ketch-controller.yaml
+kubectl apply -f https://github.com/theketchio/ketch/releases/download/v0.6.1/ketch-controller.yaml
