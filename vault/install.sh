ec7bd2e587421db9c79afb6ef60819e222bc0d23
relgisri <dennis@irsigler.cloud>
2021-11-22 13:13:23 +0100

    Update Vault version and name
    Signed-off-by: relgisri <dennis@irsigler.cloud>

--- vault/install.sh
+++ vault/install.sh
@@ -5,10 +5,9 @@ helm repo add hashicorp https://helm.releases.hashicorp.com
 
 case $INSTALL_MODE in
   dev)
-    helm upgrade --install --namespace vault  --version 0.13.0 vault hashicorp/vault --set "server.dev.enabled=true"
+    helm upgrade --install --namespace vault  --version 0.18.0 vault hashicorp/vault --set "server.dev.enabled=true"
     ;;
   standalone)
-    helm upgrade --atomic --install --namespace vault --version 0.13.0 vault hashicorp/vault
+    helm upgrade --atomic --install --namespace vault --version 0.18.0 vault hashicorp/vault
     ;;
 esac
-
