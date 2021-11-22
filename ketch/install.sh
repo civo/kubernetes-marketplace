c1d1eed34169762980f0267863e22a66ea363121
Ravi Lachhman <42775804+ravilach@users.noreply.github.com>
2021-11-22 12:09:47 -0500

    Added an additional wait on the Kubernetes Cert Manager Deployment

--- ketch/install.sh
+++ ketch/install.sh
@@ -3,5 +3,8 @@
 #Sleep for 5 Seconds incase Civo Cert Manager Install has not scaled to one replica. Ketch v0.6.1 handles this better.
 sleep 5
 
+#Incase Sleep does not work, can leverage a kubectl wait for the Cert Manager deployment to be done.
+kubectl wait --for=condition=available --timeout=300s deployment/cert-manager-webhook  -n cert-manager
+
 #Install Ketch
 kubectl apply -f https://github.com/theketchio/ketch/releases/download/v0.6.1/ketch-controller.yaml
