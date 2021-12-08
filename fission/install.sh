06fbd661c5ef65f2fbd2a12f6803c845ced6c7e1
Sanket Sudake <sanketsudake@gmail.com>
2021-12-08 13:54:21 +0530

    New App: Fission
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

---
+++ fission/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+helm repo add fission-charts https://fission.github.io/fission-charts
+helm repo update
+
+kubectl create namespace fission
+
+kubectl create -k "github.com/fission/fission/crds/v1?ref=v1.15.0"
+helm install fission fission-charts/fission-all --version v1.15.0 --namespace fission \
+    --set routerServiceType=NodePort
