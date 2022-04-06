c0bb55736674511ea245500effdcd207629f6e27
narendran <narendev0610@gmail.com>
2022-04-06 17:41:18 +0530

    fixing nginx install script for k8s v1.22

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,3 +1,3 @@
-version=$(curl -s "https://api.github.com/repos/kubernetes/ingress-nginx/releases/latest" | awk -F '"' '/tag_name/{print $4}')
+version=$(curl -s "https://api.github.com/repos/kubernetes/ingress-nginx/releases" | grep tag_name | grep controller | sort -r | awk -F'"' 'NR==1 {print $4}')
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/${version}/deploy/static/provider/cloud/deploy.yaml
