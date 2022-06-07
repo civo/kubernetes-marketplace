5d3fa9edbfbf4e1e1ddee36dc2510d1cd2cd21cf
Sanket Sudake <sanketsudake@gmail.com>
2022-06-07 15:21:43 +0530

    Update Fission version to 1.16.0
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- fission/uninstall.sh
+++ fission/uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 helm uninstall fission --namespace fission
-kubectl delete -k "github.com/fission/fission/crds/v1?ref=v1.15.1"
+kubectl delete -k "github.com/fission/fission/crds/v1?ref=v1.16.0"
 kubectl delete ns fission
