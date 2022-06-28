e913f26d6cb032773f68a1197505b7365205634c
Engin Diri <engin.diri@mail.schwarz>
2022-06-28 07:59:13 +0200

    chore: Update ArgoCD to version v2.4.2
    Signed-off-by: Engin Diri <engin.diri@mail.schwarz>

--- argocd/install.sh
+++ argocd/install.sh
@@ -5,4 +5,4 @@ helm repo update
 
 kubectl create namespace argocd
 
-helm install argo-cd argo/argo-cd --version 4.5.0 --namespace argocd
+helm install argo-cd argo/argo-cd --version 4.9.8 --namespace argocd
