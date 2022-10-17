61e8999186c37bc6b7bf2eece3cdcd1869dfbb1a
Laszlo Fogas <laszlo@gimlet.io>
2022-10-17 11:47:23 +0200

    Not using values file as it is not available

--- gimlet/install.sh
+++ gimlet/install.sh
@@ -7,6 +7,9 @@ kubectl create namespace gimlet-installer || true
 helm repo update \
  && helm upgrade gimlet-installer --install onechart/onechart \
     --namespace gimlet-installer  \
-    -f values.yaml \
+    --set image.repository=ghcr.io/gimlet-io/gimlet-installer \
+    --set tag=v0.4.2 \
+    --set containerPort=9000 \
     --set vars.HOST="$CLUSTER_ID.k8s.civo.com" \
-    --set ingress.host="gimlet.$CLUSTER_ID.k8s.civo.com"
+    --set ingress.host="gimlet.$CLUSTER_ID.k8s.civo.com" \
+    --set "ingress.annotations.kubernetes\.io/ingress\.class=traefik"
