4ce8590eb9f9c159a663fafc35d78ce65f87dcd6
Saiyam Pathak <saiyam911@gmail.com>
2022-11-30 11:13:28 +0530

    portainer BE

---
+++ portainer/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+case $EDITION in
+  ce)
+    kubectl apply -f https://raw.githubusercontent.com/civo/kubernetes-marketplace/master/portainer/ce-app.yaml
+    ;;
+  ee)  
+    kubectl apply -f https://raw.githubusercontent.com/civo/kuber
+netes-marketplace/master/portainer/ee-app.yaml
+    ;;
+esac
+
