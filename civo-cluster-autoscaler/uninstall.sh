1b5b6d96fba925ac841699ba2c1b6d164f76645f
Haardik Dharma <haardik@civo.com>
2022-12-15 14:25:03 +0530

    Update uninstall.sh

--- civo-cluster-autoscaler/uninstall.sh
+++ civo-cluster-autoscaler/uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Delete the app
-kubectl delete -f https://github.com/civo/kubernetes-marketplace/blob/master/civo-cluster-autoscaler/app.yaml
+kubectl delete -f https://raw.githubusercontent.com/civo/kubernetes-marketplace/master/civo-cluster-autoscaler/app.yaml
