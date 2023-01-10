f91fa80827d58d68d5c74d5d8e47d0ee86885b4d
Andrew Minkin <andrew.minkin@percona.com>
2023-01-10 19:39:13 +0600

    Better way to provision dbaas

--- percona-monitoring-and-management/install.sh
+++ percona-monitoring-and-management/install.sh
@@ -1,5 +1,28 @@
-helm repo add percona https://percona.github.io/percona-helm-charts/
-helm repo update
-kubectl create ns pmm
+#!/bin/bash
+PMM_PASSWORD=$(echo ${PMM_ADMIN_PASSWORD}|base64)
 
-helm install pmm percona/pmm --namespace pmm  --set secret.pmm_password=$PMM_ADMIN_PASSWORD --set service.type=$SERVICE_TYPE --set-string pmmEnv.ENABLE_DBAAS="1"
+# Creating secret
+envsubst < secret.yml|kubectl apply -f -
+
+# Add the Percona Helm repository
+helm repo add percona https://percona.github.io/percona-helm-charts/
+
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the PMM  Helm chart
+helm install pmm percona/pmm --set secret.create=false --set service.type=$SERVICE_TYPE
+
+# Wait for pod to be ready
+kubectl wait pods --for condition=Ready --timeout=120s pmm-0
+
+# Getting public address for the next step
+public_address=$(kubectl get svc monitoring-service -o jsonpath="{.status.loadBalancer.ingress[0].ip"})
+
+# Setting PMM settings to enable DBaaS correctly. It requires public address to be set
+settings_data="{\"enable_dbaas\": true, \"pmm_public_address\": \"$public_address\"}"
+
+curl -k -XPOST -u "admin:$admin_password" https://$public_address/v1/Settings/Change\
+	-H 'Content-type: application/json'\
+	-H 'Accept: application/json'\
+	--data-raw "$settings_data"
