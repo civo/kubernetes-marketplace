bc4947ca8fbb95b111952473f3caf43804ad98d9
Jintao Zhang <zhangjintao9020@gmail.com>
2023-01-11 14:06:06 +0800

    Update APISIX Ingress version
    * bump to latest version
    * set loadbalancer mode is used by default
    
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- apisix-ingress-controller/install.sh
+++ apisix-ingress-controller/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APISIX_CHART_VERSION=0.12.3
+APISIX_CHART_VERSION=1.1.0
 NS=apisix
 
 # Add the Helm repository for Apache APISIX Ingress Controller
@@ -9,4 +9,4 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 # Create the namespace and install the Helm chart
-helm upgrade -i apisix apisix/apisix --version=$APISIX_CHART_VERSION --namespace $NS --create-namespace --set ingress-controller.enabled=true --set ingress-controller.config.apisix.serviceNamespace=$NS  --set ingress-controller.config.apisix.adminAPIVersion=v3
+helm upgrade -i apisix apisix/apisix --version=$APISIX_CHART_VERSION --namespace $NS --create-namespace --set gateway.type=LoadBalancer --set ingress-controller.enabled=true --set ingress-controller.config.apisix.serviceNamespace=$NS  --set ingress-controller.config.apisix.adminAPIVersion=v3 --set ingress-controller.config.ingressPublishService="$NS/apisix-gateway"
