ad46c80144daf2afe72cc67a7a4e792816d9938a
Olivier Vernin <olivier.vernin@suse.com>
2023-02-15 17:34:19 +0100

    Update cert-manager github org
    Signed-off-by: Olivier Vernin <olivier.vernin@suse.com>

--- cert-manager/install.sh
+++ cert-manager/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.5.3/cert-manager.yaml
