c422b0d766c149bd73c93ca6e1a9a83b5183dc35
Oksana Grishchenko <91597950+oksana-grishchenko@users.noreply.github.com>
2023-03-03 10:36:01 +0200

    Update install.sh

--- percona-monitoring-and-management/install.sh
+++ percona-monitoring-and-management/install.sh
@@ -7,7 +7,7 @@ helm repo add percona https://percona.github.io/percona-helm-charts/
 helm repo update
 
 # Install the PMM  Helm chart
-helm install pmm percona/pmm --set secret.pmm_password=$PMM_ADMIN_PASSWORD --set service.type=LoadBalancer --set-string pmmEnv.ENABLE_DBAAS="1"
+helm install pmm percona/pmm --set secret.pmm_password="$PMM_ADMIN_PASSWORD" --set service.type=LoadBalancer --set-string pmmEnv.ENABLE_DBAAS="1"
 
 # Wait for pod to be ready
 kubectl wait pods --for condition=Ready --timeout=5m pmm-0
@@ -26,7 +26,7 @@ public_address=$(kubectl get svc monitoring-service -o jsonpath="{.status.loadBa
 # Setting PMM settings to enable DBaaS correctly. It requires public address to be set
 settings_data="{\"enable_dbaas\": true, \"pmm_public_address\": \"$public_address\"}"
 
-curl -k -XPOST -u 'admin:'"$PMM_ADMIN_PASSWORD"'' https://$public_address/v1/Settings/Change\
+curl -k -XPOST -u 'admin:'"$PMM_ADMIN_PASSWORD"'' https://"$public_address"/v1/Settings/Change\
 	-H 'Content-type: application/json'\
 	-H 'Accept: application/json'\
 	--data-raw "$settings_data"
