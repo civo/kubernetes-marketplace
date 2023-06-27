943b459577166dc770196e03b77d01a737483e7e
Jared Edwards <jared@kubefirst.com>
2023-06-26 23:22:09 -0600

    add ui adjustment for kubefirst console

--- kubefirst/install.sh
+++ kubefirst/install.sh
@@ -3,4 +3,4 @@
 helm repo add kubefirst https://charts.kubefirst.com
 helm repo update
 
-helm install kubefirst --create-namespace --namespace kubefirst --wait kubefirst/kubefirst
+helm install kubefirst --set=console.installMethod=civo-marketplace --create-namespace --namespace kubefirst --wait --version 2.1.12 kubefirst/kubefirst
