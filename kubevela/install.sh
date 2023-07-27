93fcaa2f1d241bd0281e7e52c1b7383f6a2e9957
qiaozp <qiaozhongpei.qzp@alibaba-inc.com>
2023-07-27 11:07:01 +0800

    Feat: bump KubeVela to 1.9.4
    Signed-off-by: qiaozp <qiaozhongpei.qzp@alibaba-inc.com>

--- kubevela/install.sh
+++ kubevela/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -fsSl https://static.kubevela.net/script/install.sh | sudo bash -s 1.5.3
+curl -fsSl https://kubevela.io/script/install.sh | sudo bash -s 1.9.4
 vela install
 
 ## enable velaux by default
