48a0ee34b57851a75260e0d85004938f80b1a261
Sander Rodenhuis <sander.rodenhuis@redkubes.com>
2023-10-18 13:49:27 +0200

    fix: kc limits

--- otomi/install.sh
+++ otomi/install.sh
@@ -7,4 +7,12 @@ helm repo update
 
 helm install otomi otomi/otomi \
     --set cluster.name="$name" \
-    --set cluster.provider=civo
+    --set cluster.provider=civo \
+    --set apps.keycloak.resources.keycloak.limits.cpu="2000m" \
+    --set apps.keycloak.resources.keycloak.limits.memory="1Gi" \
+    --set apps.keycloak.resources.keycloak.requests.cpu="500m" \
+    --set apps.keycloak.resources.keycloak.requests.memory="512Mi" \
+    --set apps.keycloak.resources.operator.limits.cpu="1000m" \
+    --set apps.keycloak.resources.operator.limits.memory="512Mi" \
+    --set apps.keycloak.resources.operator.requests.cpu="100m" \
+    --set apps.keycloak.resources.operator.requests.memory="128Mi"
