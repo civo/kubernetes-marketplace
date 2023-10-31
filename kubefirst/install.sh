50beeac8d8433bf6504cbfc2c51d3ec4f51df082
John Dietz <john@kubefirst.com>
2023-10-31 11:28:31 -0400

    pinning kubefirst chart to 2.2.16

--- kubefirst/install.sh
+++ kubefirst/install.sh
@@ -3,4 +3,4 @@
 helm repo add kubefirst https://charts.kubefirst.com
 helm repo update
 
-helm install kubefirst --set=console.installMethod=civo-marketplace --set=global.cloudProvider=civo --set=global.clusterType=bootstrap --set=global.installMethod=civo-marketplace --create-namespace --namespace kubefirst --wait kubefirst/kubefirst --version 2.3.4
+helm install kubefirst --set=console.installMethod=civo-marketplace --set=kubefirst-api.installMethod=civo-marketplace --version 2.2.16 --create-namespace --namespace kubefirst --wait kubefirst/kubefirst
