769d08e5f6d40d941b9e94e599dd404159999608
Russ Savage <russorat@users.noreply.github.com>
2023-12-04 10:14:16 -0800

    fix: adding version number
    Signed-off-by: Russ Savage <russorat@users.noreply.github.com>

--- ngrok/install.sh
+++ ngrok/install.sh
@@ -2,9 +2,10 @@
 
 NAMESPACE=ngrok-ingress-controller
 SECRET_NAME=ngrok-ingress-controller-credentials
+VERSION=$(helm search hub ngrok kubernetes-ingress-controller -o json | jq -r '.[0].version')
 
 echo "Enabling ngrok ingress controller in {$NAMESPACE} namespace"
 
 helm repo update ngrok || helm repo add ngrok https://ngrok.github.io/kubernetes-ingress-controller
 
-helm upgrade --install --create-namespace ngrok-ingress-controller ngrok/kubernetes-ingress-controller --namespace "${NAMESPACE}" --set credentials.secret.name="${SECRET_NAME}"
+helm upgrade --install --create-namespace ngrok-ingress-controller ngrok/kubernetes-ingress-controller --namespace "${NAMESPACE}" --set credentials.secret.name="${SECRET_NAME}" --version $VERSION
