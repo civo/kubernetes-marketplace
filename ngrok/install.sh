72a371c0a0f392a35eb4d1961b0ae0f3b1dc6982
Saiyam Pathak <saiyam911@gmail.com>
2023-12-05 21:18:12 +0530

    Revert "fix: adding version number"
    This reverts commit 769d08e5f6d40d941b9e94e599dd404159999608.

--- ngrok/install.sh
+++ ngrok/install.sh
@@ -2,10 +2,9 @@
 
 NAMESPACE=ngrok-ingress-controller
 SECRET_NAME=ngrok-ingress-controller-credentials
-VERSION=$(helm search hub ngrok kubernetes-ingress-controller -o json | jq -r '.[0].version')
 
 echo "Enabling ngrok ingress controller in {$NAMESPACE} namespace"
 
 helm repo update ngrok || helm repo add ngrok https://ngrok.github.io/kubernetes-ingress-controller
 
-helm upgrade --install --create-namespace ngrok-ingress-controller ngrok/kubernetes-ingress-controller --namespace "${NAMESPACE}" --set credentials.secret.name="${SECRET_NAME}" --version $VERSION
+helm upgrade --install --create-namespace ngrok-ingress-controller ngrok/kubernetes-ingress-controller --namespace "${NAMESPACE}" --set credentials.secret.name="${SECRET_NAME}"
