e0f966a4517137ecdc1e75c984c48c90f528d8a6
Saiyam Pathak <saiyam@civo.com>
2023-12-12 23:09:04 +0530

    Update install.sh

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 version=$(curl -s "https://api.github.com/repos/kubernetes/ingress-nginx/releases" | grep tag_name | grep controller | sort -r | awk -F'"' 'NR==1 {print $4}')
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/${version}/deploy/static/provider/cloud/deploy.yaml
