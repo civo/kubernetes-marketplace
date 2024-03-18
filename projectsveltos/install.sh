c4cad3f24a4b328bafc249ac7d0eb4773b747a36
Saiyam Pathak <saiyam911@gmail.com>
2024-03-18 19:09:36 +0100

    Revert "Add Projectsveltos"
    This reverts commit 46f1f460fe8fcce52d7e00c56cfe611b4688469b.

--- projectsveltos/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-helm repo add projectsveltos https://projectsveltos.github.io/helm-charts
-
-helm repo update
-
-helm install projectsveltos projectsveltos/projectsveltos -n projectsveltos --create-namespace --version v0.25.0
