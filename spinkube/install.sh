3d5dbbfee18a6d56db8dd8c9091f77685b63cc86
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2024-04-11 12:51:22 +0200

    Update SpinKube to wait for cert-manager
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

--- spinkube/install.sh
+++ spinkube/install.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 #Cert manager check 
+#If namespace, or deployment is not found, the wait command below will fail, and the script continues. 
+until $(kubectl get namespaces cert-manager -o name --ignore-not-found | grep -q "namespace/cert-manager"); do 
+    echo "Waiting for cert-manager namespace creation"; 
+    sleep 1;
+done
 
-kubectl wait --for=condition=available --timeout=300s deployment/cert-manager-webhook  -n cert-manager
+until $(kubectl get deployment cert-manager-webhook -n cert-manager --ignore-not-found -o name | grep -q "deployment.apps/cert-manager-webhook"); do
+    echo "Waiting for cert-manager-webhook deployment";
+    sleep 1;
+done
+
+kubectl wait --for=condition=available --timeout=300s deployment/cert-manager-webhook  -n cert-manager 
 
 kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.runtime-class.yaml
 kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.crds.yaml
