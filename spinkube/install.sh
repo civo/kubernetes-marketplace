d96001d8c467c96c94b77f9691d5335322035379
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2024-04-22 13:04:11 +0200

    Talos only, bumping version
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

--- spinkube/install.sh
+++ spinkube/install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#Check we are on Talos
+if $(kubectl get nodes -o json | jq .items.[].status.nodeInfo.osImage | grep -q "Talos"); then 
+    echo "Found Talos as part of OS Image string"; else
+    echo "Did not find Talos as part of the OS Image string. Exiting as only Talos is supported" ;
+    exit
+fi
+
 #Cert manager check 
 #If namespace, or deployment is not found, the wait command below will fail, and the script continues. 
 until $(kubectl get namespaces cert-manager -o name --ignore-not-found | grep -q "namespace/cert-manager"); do 
