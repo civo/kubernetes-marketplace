da9af27860983b3992d55ff7097d9d536fb3ba13
Danielle Lancashire <dani@builds.terrible.systems>
2024-05-21 15:22:42 +0200

    spinkube: bump to 0.2.0

--- spinkube/install.sh
+++ spinkube/install.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
+
+SPINKUBE_VERSION="0.2.0"
+
 #Check we are on Talos
-if $(kubectl get nodes -o json | jq .items.[].status.nodeInfo.osImage | grep -q "Talos"); then 
+if $(kubectl get nodes -o json | jq .items.[].status.nodeInfo.osImage | grep -q "Talos"); then
     echo "Found Talos as part of OS Image string"; else
     echo "Did not find Talos as part of the OS Image string. Exiting as only Talos is supported" ;
     exit
 fi
 
-#Cert manager check 
-#If namespace, or deployment is not found, the wait command below will fail, and the script continues. 
-until $(kubectl get namespaces cert-manager -o name --ignore-not-found | grep -q "namespace/cert-manager"); do 
-    echo "Waiting for cert-manager namespace creation"; 
+#Cert manager check
+#If namespace, or deployment is not found, the wait command below will fail, and the script continues.
+until $(kubectl get namespaces cert-manager -o name --ignore-not-found | grep -q "namespace/cert-manager"); do
+    echo "Waiting for cert-manager namespace creation";
     sleep 1;
 done
 
@@ -18,14 +21,14 @@ until $(kubectl get deployment cert-manager-webhook -n cert-manager --ignore-not
     sleep 1;
 done
 
-kubectl wait --for=condition=available --timeout=300s deployment/cert-manager-webhook  -n cert-manager 
+kubectl wait --for=condition=available --timeout=300s deployment/cert-manager-webhook  -n cert-manager
 
-kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.runtime-class.yaml
-kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.crds.yaml
-kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.shim-executor.yaml
+kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v${SPINKUBE_VERSION}/spin-operator.runtime-class.yaml
+kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v${SPINKUBE_VERSION}/spin-operator.crds.yaml
+kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v${SPINKUBE_VERSION}/spin-operator.shim-executor.yaml
 helm install spin-operator \
   --namespace spin-operator \
   --create-namespace \
-  --version 0.1.0 \
+  --version ${SPINKUBE_VERSION} \
   --wait \
   oci://ghcr.io/spinkube/charts/spin-operator
