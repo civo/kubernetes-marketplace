0637b9770bfa6c6111641406c741496f840cd512
Danielle Lancashire <dani@builds.terrible.systems>
2024-05-21 15:22:42 +0200

    spinkube: bump to 0.2.0

--- spinkube/uninstall.sh
+++ spinkube/uninstall.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+SPINKUBE_VERSION="0.2.0"
+
 # Uninstall Spin Operator using Helm
 helm uninstall spin-operator --namespace spin-operator
 
 # Uninstall all resources related to spin-operator including CRD resources
-kubectl delete -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.runtime-class.yaml
-kubectl delete -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.crds.yaml
-kubectl delete -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.shim-executor.yaml
+kubectl delete -f https://github.com/spinkube/spin-operator/releases/download/v${SPINKUBE_VERSION}/spin-operator.runtime-class.yaml
+kubectl delete -f https://github.com/spinkube/spin-operator/releases/download/v${SPINKUBE_VERSION}/spin-operator.crds.yaml
+kubectl delete -f https://github.com/spinkube/spin-operator/releases/download/v${SPINKUBE_VERSION}/spin-operator.shim-executor.yaml
 
 # Delete Namespace
 kubectl delete ns spin-operator
