53fa1f14c26a8a0a82aab9e5473dbc9760bb51c6
nishitm060@gmail.com <nishitm060@gmail.com>
2024-09-03 09:24:35 +0530

    Issue:#522 Updated version Reloader to new

--- reloader/install.sh
+++ reloader/install.sh
@@ -3,4 +3,4 @@
 helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo update
 
-helm install reloader stakater/reloader --version v0.0.125 --namespace reloader --create-namespace
+helm install reloader stakater/reloader --version v1.1.0 --namespace reloader --create-namespace
