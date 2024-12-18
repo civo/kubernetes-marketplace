7ed021aed6d762a9533659f3fd2555a6f04e5778
mrrishi <mrrishi373@gmail.com>
2024-12-19 04:28:25 +0530

    feat: upgrade version

--- kubefirst/install.sh
+++ kubefirst/install.sh
@@ -3,4 +3,4 @@
 helm repo add konstruct https://charts.konstruct.io
 helm repo update
 
-helm install kubefirst --set=console.installMethod=civo-marketplace --set=global.cloudProvider=civo --set=global.clusterType=bootstrap --set=global.installMethod=civo-marketplace --set=console.ingress.enabled=false  --create-namespace --namespace kubefirst --version 2.6.1 --wait konstruct/kubefirst
+helm install kubefirst --set=console.installMethod=civo-marketplace --set=global.cloudProvider=civo --set=global.clusterType=bootstrap --set=global.installMethod=civo-marketplace --set=console.ingress.enabled=false  --create-namespace --namespace kubefirst --version 2.7.9 --wait konstruct/kubefirst
