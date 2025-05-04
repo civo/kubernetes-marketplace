718f55ca4195147e734bdef11f56e9653ac9393a
Dinesh Majrekar <dinesh@civo.com>
2025-05-04 07:41:54 +0100

    update test

--- redis/conformance.sh
+++ redis/conformance.sh
@@ -4,6 +4,6 @@
 apt update && apt install -y redis-tools
 
 # Run conformance test
-redis-cli -h redis -a $REDIS_PASS SET testkey "Hello, Redis!"
-redis-cli -h redis -a $REDIS_PASS GET testkey
-redis-cli -h redis -a $REDIS_PASS EXISTS testkey
+redis-cli -h redis SET testkey "Hello, Redis!"
+redis-cli -h redis GET testkey
+redis-cli -h redis EXISTS testkey
