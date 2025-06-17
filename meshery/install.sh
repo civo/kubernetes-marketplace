9e48514626ad8eba668e77ce90feae72bea020a2
Yash sharma <71271069+Yashsharma1911@users.noreply.github.com>
2025-06-17 15:24:54 +0530

    Update version in menifest

--- meshery/install.sh
+++ meshery/install.sh
@@ -4,4 +4,4 @@ helm repo add meshery https://meshery.io/charts/
 
 helm repo update
 
-helm install meshery meshery/meshery --namespace meshery --create-namespace
+helm install meshery meshery/meshery --namespace meshery --create-namespace --version v0.8.103
