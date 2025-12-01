dea9f890169010e5a3cc2bc4a84377f815d304df
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2025-12-01 18:03:52 +0900

    fix: kafka - image name
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- kafka/install.sh
+++ kafka/install.sh
@@ -6,4 +6,4 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 # Installing kafka with default rules.
-helm install kafka -f https://raw.githubusercontent.com/bitnami/charts/refs/heads/main/bitnami/kafka/values.yaml bitnami/kafka -n kafka
+helm install kafka -f https://raw.githubusercontent.com/bitnami/charts/refs/heads/main/bitnami/kafka/values.yaml bitnami/kafka --set image.repository=bitnamilegacy/kafka -n kafka
