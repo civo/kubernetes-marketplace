d33a6e0999c1a9e7079b30f14bccddfd5b8ca375
uartnet <140632163+uartnet@users.noreply.github.com>
2026-08-05 12:49:58 +0200

    add rstream operator

---
+++ rstream-operator/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+NAMESPACE="rstream-system"
+VERSION="0.5.1"
+
+echo "Installing rstream operator in ${NAMESPACE} namespace"
+
+helm upgrade --install --create-namespace rstream-operator \
+  oci://ghcr.io/rstreamlabs/rstream-operator \
+  --namespace "${NAMESPACE}" \
+  --version "${VERSION}" \
+  --wait \
+  --timeout 8m
